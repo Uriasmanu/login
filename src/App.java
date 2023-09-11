74384b40cbf28979293c57db0df36d48f1e959d2
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-11 20:01:50 -0300

    s

--- src/App.java
+++ src/App.java
@@ -4,14 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
        String login, senha;
 
-    JOptionPane.showMessageDialog(null,"Seja bem-vindo(a) ao sistema da Loja SPCar");
-
     login = JOptionPane.showInputDialog("Digite o seu login");
     senha = JOptionPane.showInputDialog("Digite a sua senha");
 
     if (login.equals("aluno") && senha.equals("aluno")) {
         JOptionPane.showMessageDialog(null,"Acesso Liberado");
-        
+
         }else{
             JOptionPane.showMessageDialog(null,"Login ou senha incorretos");
         }
